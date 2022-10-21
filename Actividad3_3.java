e0eee33ac1295fe3cb6f8c3939a2c779b55f272c
PabloMateosP <pmatpal0105@g.educaand.es>
2022-10-21 12:49:33 +0200

    Update Actividad3_3.java
    Punto2

--- Actividad3_3.java
+++ Actividad3_3.java
@@ -2,5 +2,8 @@ public class Actividad3_3 {
     public static void main(String[] args) {
         //Punto 1
         System.out.println("Hola Mundo");
+        //Punto 2
+        String nombre = "Pablo";
+        System.out.printf("Hola %s",nombre);
     }
 }
